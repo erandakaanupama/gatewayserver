a61cdfaf0ca09a519f85522c9f270b83d6f0f2fd
erandaka.bandara <erandakaanupama@gmail.com>
2026-06-20 21:51:48 +0530

    test(gatewayserver): add application context load test

---
+++ src/test/kotlin/com/diningplate/gatewayserver/GatewayserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.diningplate.gatewayserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
